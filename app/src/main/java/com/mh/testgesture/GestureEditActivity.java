aca4305171cb783ca2faa7a3b6cebf578a36a4df
user <yuehui1016@qq.com>
2016-07-12 17:56:41 +0800

    DDDD

--- app/src/main/java/com/mh/testgesture/GestureEditActivity.java
+++ app/src/main/java/com/mh/testgesture/GestureEditActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.Animation;
@@ -14,129 +15,137 @@ import android.widget.Toast;
 
 import com.mh.testgesture.widget.GestureContentView;
 import com.mh.testgesture.widget.GestureDrawline;
-import com.mh.testgesture.widget.LockIndicator;
 
 
 /**
- * 
  * 手势密码设置界面
- *
  */
 public class GestureEditActivity extends Activity implements OnClickListener {
-	/** 手机号码*/
-	public static final String PARAM_PHONE_NUMBER = "PARAM_PHONE_NUMBER";
-	/** 意图*/
-	public static final String PARAM_INTENT_CODE = "PARAM_INTENT_CODE";
-	/** 首次提示绘制手势密码，可以选择跳过 */
-	public static final String PARAM_IS_FIRST_ADVICE = "PARAM_IS_FIRST_ADVICE";
-	private TextView mTextTitle;
-	private TextView mTextCancel;
-	private LockIndicator mLockIndicator;
-	private TextView mTextTip;
-	private FrameLayout mGestureContainer;
-	private GestureContentView mGestureContentView;
-	private TextView mTextReset;
-	private String mParamSetUpcode = null;
-	private String mParamPhoneNumber;
-	private boolean mIsFirstInput = true;
-	private String mFirstPassword = null;
-	private String mConfirmPassword = null;
-	private int mParamIntentCode;
+    /**
+     * 手机号码
+     */
+    public static final String PARAM_PHONE_NUMBER = "PARAM_PHONE_NUMBER";
+    /**
+     * 意图
+     */
+    public static final String PARAM_INTENT_CODE = "PARAM_INTENT_CODE";
+    /**
+     * 首次提示绘制手势密码，可以选择跳过
+     */
+    public static final String PARAM_IS_FIRST_ADVICE = "PARAM_IS_FIRST_ADVICE";
+    private TextView mTextTip;
+    private FrameLayout mGestureContainer;
+    private GestureContentView mGestureContentView;
+    private TextView mTextReset;
+    private String mParamSetUpcode = null;
+    private String mParamPhoneNumber;
+    private boolean mIsFirstInput = true;
+    private String mFirstPassword = null;
+    private String mConfirmPassword = null;
+    private int mParamIntentCode;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_gesture_edit);
-		setUpViews();
-		setUpListeners();
-	}
-	
-	private void setUpViews() {
-		mTextTitle = (TextView) findViewById(R.id.text_title);
-		mTextCancel = (TextView) findViewById(R.id.text_cancel);
-		mTextReset = (TextView) findViewById(R.id.text_reset);
-		mTextReset.setClickable(false);
-		mLockIndicator = (LockIndicator) findViewById(R.id.lock_indicator);
-		mTextTip = (TextView) findViewById(R.id.text_tip);
-		mGestureContainer = (FrameLayout) findViewById(R.id.gesture_container);
-		// 初始化一个显示各个点的viewGroup
-		mGestureContentView = new GestureContentView(this, false, "", new GestureDrawline.GestureCallBack() {
-			@Override
-			public void onGestureCodeInput(String inputCode) {
-				if (!isInputPassValidate(inputCode)) {
-					mTextTip.setText(Html.fromHtml("<font color='#c70c1e'>至少链接4个点, 请重新绘制密码</font>"));
-					mGestureContentView.clearDrawlineState(0L);
-					return;
-				}
-				if (mIsFirstInput) {
-					mFirstPassword = inputCode;
-					updateCodeList(inputCode);
-					mGestureContentView.clearDrawlineState(0L);
-					mTextReset.setClickable(true);
-					mTextReset.setText(getString(R.string.reset_gesture_code));
-				} else {
-					if (inputCode.equals(mFirstPassword)) {
-							Toast.makeText(GestureEditActivity.this, "设置成功", Toast.LENGTH_SHORT).show();
-						mGestureContentView.clearDrawlineState(0L);
-						GestureEditActivity.this.finish();
-					} else {
-						mTextTip.setText(Html.fromHtml("<font color='#c70c1e'>与上一次绘制不一致，请重新绘制</font>"));
-						// 左右移动动画
-						Animation shakeAnimation = AnimationUtils.loadAnimation(GestureEditActivity.this, R.anim.shake);
-						mTextTip.startAnimation(shakeAnimation);
-						// 保持绘制的线，1.5秒后清除
-						mGestureContentView.clearDrawlineState(1300L);
-					}
-				}
-				mIsFirstInput = false;
-			}
 
-			@Override
-			public void checkedSuccess() {
-				
-			}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gesture_edit);
+        setUpViews();
+        setUpListeners();
+    }
 
-			@Override
-			public void checkedFail() {
-				
-			}
-		});
-		// 设置手势解锁显示到哪个布局里面
-		mGestureContentView.setParentView(mGestureContainer);
-		updateCodeList("");
-	}
-	
-	private void setUpListeners() {
-		mTextCancel.setOnClickListener(this);
-		mTextReset.setOnClickListener(this);
-	}
-	
-	private void updateCodeList(String inputCode) {
-		// 更新选择的图案
-		mLockIndicator.setPath(inputCode);
-	}
+    private void setUpViews() {
+        //跳过
+        mTextReset = (TextView) findViewById(R.id.text_reset);
+        mTextReset.setClickable(false);
+        mTextTip = (TextView) findViewById(R.id.text_tip);
+        mGestureContainer = (FrameLayout) findViewById(R.id.gesture_container);
+        // 初始化一个显示各个点的viewGroup
+        mGestureContentView = new GestureContentView(this, false, "", new GestureDrawline.GestureCallBack() {
+            @Override
+            public void onGestureCodeInput(String inputCode) {
+                if (!isInputPassValidate(inputCode)) {
+                    mTextTip.setText(Html.fromHtml("<font color='#DA3412'>至少链接4个点, 请重新绘制密码</font>"));
+                    mGestureContentView.clearDrawlineState(0L);
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.text_cancel:
-			this.finish();
-			break;
-		case R.id.text_reset:
-			mIsFirstInput = true;
-			updateCodeList("");
-			mTextTip.setText(getString(R.string.set_gesture_pattern));
-			break;
-		default:
-			break;
-		}
-	}
-	
-	private boolean isInputPassValidate(String inputPassword) {
-		if (TextUtils.isEmpty(inputPassword) || inputPassword.length() < 4) {
-			return false;
-		}
-		return true;
-	}
-	
+
+                    return;
+                }
+                if (mIsFirstInput) {
+                    mFirstPassword = inputCode;
+                    updateCodeList(inputCode);
+                    mGestureContentView.clearDrawlineState(0L);
+                    mTextReset.setClickable(true);
+                    mTextReset.setText(getString(R.string.reset_gesture_code));
+                    mTextTip.setText("确认手势密码");
+                } else {
+                    if (inputCode.equals(mFirstPassword)) {
+                        Toast.makeText(GestureEditActivity.this, "设置成功", Toast.LENGTH_SHORT).show();
+                        mGestureContentView.clearDrawlineState(0L);
+                        //在此处将用户输入的手势密码存起来
+                        Log.d("蜡笔小新", inputCode);
+
+
+                        GestureEditActivity.this.finish();
+                    } else {
+                        mTextTip.setText(Html.fromHtml("<font color='#DA3412'>与上一次绘制不一致，请重新绘制</font>"));
+                        // 左右移动动画
+                        Animation shakeAnimation = AnimationUtils.loadAnimation(GestureEditActivity.this, R.anim.shake);
+                        mTextTip.startAnimation(shakeAnimation);
+                        // 保持绘制的线，1.5秒后清除
+                        mGestureContentView.clearDrawlineState(1300L);
+
+
+                    }
+                }
+                mIsFirstInput = false;
+            }
+
+            @Override
+            public void checkedSuccess() {
+
+
+            }
+
+            @Override
+            public void checkedFail() {
+
+
+            }
+        });
+        // 设置手势解锁显示到哪个布局里面
+        mGestureContentView.setParentView(mGestureContainer);
+        updateCodeList("");
+    }
+
+    private void setUpListeners() {
+        mTextReset.setOnClickListener(this);
+    }
+
+    private void updateCodeList(String inputCode) {
+        // 更新选择的图案
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+//            case R.id.text_cancel:
+//                this.finish();
+//                break;
+            case R.id.text_reset:
+                mIsFirstInput = true;
+                updateCodeList("");
+                mTextTip.setText(getString(R.string.set_gesture_pattern));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private boolean isInputPassValidate(String inputPassword) {
+        if (TextUtils.isEmpty(inputPassword) || inputPassword.length() < 4) {
+            return false;
+        }
+        return true;
+    }
+
 }
